9a8830f695bcc87a094ec5607bf14a4ea95019a4
Xi Wang <wangxi8779@hotmail.com>
2022-10-17 23:00:01 +1030

    fix odds_evens input

--- odds_evens01.c
+++ odds_evens01.c
@@ -3,14 +3,14 @@
 
 int main() {
   int number;
-  printf("How many number you want to read in: ");
+ 
   scanf("%d", &number);
   int *numbers = malloc(sizeof(int) * number);
   int *evens = malloc(sizeof(int) * number);
   int *odds = malloc(sizeof(int) * number);
   int evens_index = 0;
   int odds_index = 0;
-  printf("enter the numbers:\n");
+   
 
   for (int i = 0; i < number; i++) {
     scanf("%d", &numbers[i]);
@@ -33,7 +33,7 @@ int main() {
   }
   printf("\n");
   
-  free(number);
+  free(numbers);
   free(evens);
   free(odds);
   
